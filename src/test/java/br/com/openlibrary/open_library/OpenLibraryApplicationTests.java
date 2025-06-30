2bf39871afa0cdaf7cecb7b72ea56eff658362fa
rafaelqueiroz <rafaelqsantos33@gmail.com>
2025-06-29 21:41:07 -0300

    init commit

---
+++ src/test/java/br/com/openlibrary/open_library/OpenLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.openlibrary.open_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
